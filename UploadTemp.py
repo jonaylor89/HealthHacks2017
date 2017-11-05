5a66d2cd172b01332c4a94e2db81572d3ce2c2b5
John Naylor <jonaylor89@gmail.com>
2017-11-04 20:01:29 -0400

    Let's see if this works

--- UploadTemp.py
+++ UploadTemp.py
@@ -10,8 +10,8 @@ from temp_read import read_temperature
 url = 'https://healthhacks2017-ec6bf.firebaseio.com/'
 
 postdata = {
-    'date' : str(calendar.timegm(time.gmtime())),
-    'temp' : str(read_temperature())
+    'date' : bytes(calendar.timegm(time.gmtime()), 'utf-8'),
+    'temp' : bytes(read_temperature(), 'utf-8')
 }
 
 req = Request(url)
