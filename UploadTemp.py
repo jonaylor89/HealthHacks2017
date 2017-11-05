bec76326403af71ada928bb53fc2d36444cb769e
John Naylor <jonaylor89@gmail.com>
2017-11-04 21:03:33 -0400

    Bug Fix

--- UploadTemp.py
+++ UploadTemp.py
@@ -16,6 +16,6 @@ firebase_url = 'https://healthhacks2017-ec6bf.firebaseio.com/'
 
 time_hhmmss = time.strftime('%H:%M:%S')
 
- data = {'time':time_hhmmss,'data': read_temperature()}
+data = {'time':time_hhmmss,'data': read_temperature()}
 
- result = requests.post(firebase_url + '/temperature.json', data=json.dumps(data))
+result = requests.post(firebase_url + '/temperature.json', data=json.dumps(data))
