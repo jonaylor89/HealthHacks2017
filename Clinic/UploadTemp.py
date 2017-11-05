a2d5b32ade5a19261c204d153f77bfe4af6dac0d
John Naylor <jonaylor89@gmail.com>
2017-11-05 03:22:28 -0500

    Room Num

--- Clinic/UploadTemp.py
+++ Clinic/UploadTemp.py
@@ -12,6 +12,6 @@ firebase_url = 'https://healthhacks2017-ec6bf.firebaseio.com/'
 
 time_hhmmss = time.strftime('%H:%M:%S')
 
-data = {'time':time_hhmmss,'data': read_temperature(), 'patient': patient_number}
+data = {'time':time_hhmmss,'data': read_temperature(), 'room_number': patient_number}
 
 result = requests.post(firebase_url + '/temperature.json', data=json.dumps(data))
