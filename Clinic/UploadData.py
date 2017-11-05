a0bccc96fbf3b9d732caccdfb23868e85711d83f
John Naylor <jonaylor89@gmail.com>
2017-11-05 08:02:49 -0500

    Bug Fix

--- Clinic/UploadData.py
+++ Clinic/UploadData.py
@@ -13,8 +13,6 @@ from temp_read import read_temperature
 room_number = sys.argv[1]
 Age = sys.argv[2]
 
-now = datetime.datetime.now()
-
 firebase_url = 'https://healthhacks2017-ec6bf.firebaseio.com/Clinic/'
 
 time_hhmmss = time.strftime('%H:%M:%S')
@@ -23,7 +21,7 @@ data = {'time':time_hhmmss,
         'age': int(Age),
         'sex': 'Male',
         'temperature': read_temperature(),
-        'SpO2': 0.05 * math.sin(now()) + 0.97,
+        'SpO2': 0.05 * random.randrange(-100, 100) + 0.97,
         'HR': random.randrange(70, 90),
         'systolic BP': random.choice([90, 100, 95, 91, 150, 110, 96, 93, 98, 100, 89])}
 
