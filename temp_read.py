0c6314ca777c8b86a2a6db4251e55572c647976f
John Naylor <jonaylor89@gmail.com>
2017-11-04 21:06:46 -0400

    In celsius

--- temp_read.py
+++ temp_read.py
@@ -7,6 +7,8 @@ from time import sleep
 BASE_DIR_STRING = '/sys/bus/w1/devices/'
 DEVICE_FILE_PATH_STRING = '28-80000008d65c/w1_slave'
 
+def to_fahr(cel):
+    return cel * 1.8 + 32
 
 def read_file(filepathstring):
     with open(filepathstring, 'r') as infile:
@@ -20,8 +22,8 @@ def read_temperature():
         temperature_start_index_int = wholefilestring.find('t=')
         if temperature_start_index_int != -1:
             currenttempstring = wholefilestring[temperature_start_index_int + 2:]
-            return currenttempstring.strip()
+            return to_fahr(currenttempstring.strip())
 
 
 if __name__ == "__main__":
-    print(str(calendar.timegm(time.gmtime())) + ',' + str(read_temp()))
+    print(str(calendar.timegm(time.gmtime())) + ',' + str(read_temperature()))
