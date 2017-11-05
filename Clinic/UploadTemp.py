51ae0a01d5635d32150135cdaa2c1b39d8d6f1cb
John Naylor <jonaylor89@gmail.com>
2017-11-05 03:21:47 -0500

    Patient Number added

--- Clinic/UploadTemp.py
+++ Clinic/UploadTemp.py
@@ -1,15 +1,17 @@
 
+import sys
 import time
 import requests
 import json
 
-from temp_read import read_temperature
+from ../Client/temp_read import read_temperature
 
+patient_number = sys.argv[1]
 
 firebase_url = 'https://healthhacks2017-ec6bf.firebaseio.com/'
 
 time_hhmmss = time.strftime('%H:%M:%S')
 
-data = {'time':time_hhmmss,'data': read_temperature()}
+data = {'time':time_hhmmss,'data': read_temperature(), 'patient': patient_number}
 
 result = requests.post(firebase_url + '/temperature.json', data=json.dumps(data))
